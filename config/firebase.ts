ff1f31d30d91a14dfb9a8abb0b3cfe6b0349d3ff
Deepom5 <64247104+Deepom5@users.noreply.github.com>
2024-08-21 13:57:39 +0530

    Redact the cred

--- config/firebase.ts
+++ config/firebase.ts
@@ -5,14 +5,7 @@ import {getAuth} from 'firebase/auth';
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyA3tmPjTq1wSQ1lkmlql_YPqb1wBBCedx8",
-  authDomain: "test1-6220a.firebaseapp.com",
-  databaseURL: "https://test1-6220a.firebaseio.com",
-  projectId: "test1-6220a",
-  storageBucket: "test1-6220a.appspot.com",
-  messagingSenderId: "774189415973",
-  appId: "1:774189415973:web:abad825bf087dcd96c49cc",
-  measurementId: "G-HQY8H9Z28W"
+//put your firebase config here
 };
 
 // Initialize Firebase
